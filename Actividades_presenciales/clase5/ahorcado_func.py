88e47263936d86c0e51259c2045635ed6fc8a367
Facu Ramirez <ramirezfacundoo01@gmail.com>
2025-09-30 23:00:35 -0300

    Update files

--- Actividades_presenciales/clase5/ahorcado_func.py
+++ Actividades_presenciales/clase5/ahorcado_func.py
@@ -2,7 +2,7 @@
 
 # Función para elegir palabra
 def elegir_p(palabras):
-    n = random.randit(0, len(palabras))
+    n = random.randint(0, len(palabras))
     return n
 
 # ---------- Programa Principal ----------
@@ -12,6 +12,27 @@ palabras = ["JAVA", "JavaScript", "Python", "PHP", "SQL", "Ruby", "Swift", "Rust
 letras = []
 intentos = 6
 
-p_elegida = palabras[elegir_p(palabras)]
+p_elegida = palabras[elegir_p(palabras)].upper()
+print("BIENVENIDO AL AHORCADO!")
+
 while True:
-    ing_l = input("Ingrese una letra: ").lower()
+    if intentos != 0:
+        for i in range(len(p_elegida)):
+            print("_ ", end="")
+        print("")
+        ing_l = input("Ingrese una letra: ").upper()
+        if ing_l in p_elegida:
+            print(f'Haz adivinado la letra {ing_l}!')
+            for i in range(len(p_elegida)):
+                if i == ing_l:
+                    print(f'{ing_l} ')
+                else:
+                    print("_ ")
+        else:
+            print("\nLa letra no se encuentra en la palabra!")
+            intentos -= 1
+            print(f'Intentos disponibles: {intentos}\n')
+    else:
+        print("Perdiste! :(")
+        print("Se han acabado los intentos!")
+        break
