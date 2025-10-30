561182deebd8a0830c4e81d04dc18c95a887a07c
Facu Ramirez <ramirezfacundoo01@gmail.com>
2025-10-30 12:21:03 -0300

    TP Recursividad

---
+++ TPs_Aula_Virtual/tp9/recursividad.py
@@ -0,0 +1,49 @@
+# 1) Calcular factorial de un número y los que se encuentran dentro:
+print("===============")
+print("  Ejercicio 1  ")
+print("===============")
+
+def factorial(num):
+    if num == 0:
+        return 1
+    else:
+        return num * factorial(num - 1)
+
+num = int(input("Ingrese un número entero positivo: "))
+for i in range(1, num + 1): 
+    print(f'El factorial de {i} es: {factorial(i)}')
+
+# 2)
+print("\n===============")
+print("  Ejercicio 2  ")
+print("===============")
+
+# 3)
+print("\n===============")
+print("  Ejercicio 3  ")
+print("===============")
+
+# 4)
+print("\n===============")
+print("  Ejercicio 4  ")
+print("===============")
+
+# 5)
+print("\n===============")
+print("  Ejercicio 5  ")
+print("===============")
+
+# 6)
+print("\n===============")
+print("  Ejercicio 6  ")
+print("===============")
+
+# 7)
+print("\n===============")
+print("  Ejercicio 7  ")
+print("===============")
+
+# 8)
+print("\n===============")
+print("  Ejercicio 8  ")
+print("===============")
